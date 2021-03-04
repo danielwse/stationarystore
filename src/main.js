39b2d80b05f6cca1d45f84f642a1fc948b16f3f8
danielwse <danieltzy@hotmail.com>
2021-03-04 16:24:54 +0800

    First Push

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+//Register globally
+//import CustomButton from './components/CustomButton'
+
+//Register globally
+//Vue.component('custBtn',CustomButton)
 
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
